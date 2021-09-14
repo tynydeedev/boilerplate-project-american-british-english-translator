3774917fad64d80ffa49448248098f0da1b9ee3f
tynydeedev <88718743+tynydeedev@users.noreply.github.com>
2021-09-14 13:36:27 +0000

    second commit

--- components/translator.js
+++ components/translator.js
@@ -1,33 +1,43 @@
 const americanOnly = require('./american-only.js');
+const americanArray = Object.keys(americanOnly);
+
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
+const spellingArray = Object.keys(americanToBritishSpelling);
+
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const titleArray = Object.keys(americanToBritishTitles);
+
 const britishOnly = require('./british-only.js')
+const britishArray = Object.keys(britishOnly);
 
 class Translator {
   americanInput(input) {
-    const notationRegex = /[^a-zA-Z0-9]+$/;
-    let notation = '';
-    let sentence = '';
-    if (notationRegex.test(input)) {
-      notation = input.match(notationRegex)[0];
-      sentence = input.slice(0, input.length - notation.length);
+    const sentence = input;
+
+    let returnSentence = sentence;
+    // replace the words by iterate through the arrays and find matches
+    for (let i = 0; i < americanArray.length; i++) {
+      const regex = new RegExp(`\\s${americanArray[i]}(?=[\\s\\.])`, 'i');
+      if (regex.test(returnSentence)) returnSentence = returnSentence.replace(regex, ` <span class='highlight'>${americanOnly[americanArray[i]]}</span> `);
     }
-    sentence = input;
-    const words = sentence.split(' ');
-    const converted = words.map(word => {
-      switch (true) {
-        case americanOnly.hasOwnProperty(word):
-          return `<span class="highlight">${americanOnly[word]}</span>`;
-        case americanToBritishSpelling.hasOwnProperty(word):
-          return `<span class="highlight">${americanToBritishSpelling[word]}</span>`;
-        case americanToBritishTitles.hasOwnProperty(word):
-          return `<span class="highlight">${americanToBritishTitles[word]}</span>`;
+    for (let i = 0; i < spellingArray.length; i++) {
+      const regex = new RegExp(`\\s${spellingArray[i]}\\s`, 'i');
+      if (regex.test(returnSentence)) returnSentence = returnSentence.replace(regex, ` <span class='highlight'>${americanToBritishSpelling[spellingArray[i]]}</span> `);
+    }
+    for (let i = 0; i < titleArray.length; i++) {
+      const regex = new RegExp(`\\s${titleArray[i]}\\s`, 'i');
+      if (regex.test(returnSentence)) {
+        const title = returnSentence.match(regex)[0];
+        returnSentence = returnSentence.replace(regex, ` <span class='highlight'>${title.slice(0, title.length - 1)}</span> `);
       }
-      return word;
-    });
-    const returnSentence = converted.join(' ');
-    if (sentence === returnSentence) return 'No change'
-    return returnSentence + notation;
+    }
+    
+    if (/^[a-z]/.test(returnSentence)) {
+      returnSentence = returnSentence.charAt(0).toUpperCase() + returnSentence.slice(1);
+    }
+    console.log(returnSentence);
+    if (sentence === returnSentence) return 'No change';
+    return returnSentence;
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,55 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator();
 
 suite('Unit Tests', () => {
+  suite('American to British', () => {
+    test('1st sentence', () => {
+      const output = translator.americanInput('Mangoes are my favorite fruit.');
+      assert.include(output, 'favourite', 'should change favorite to favourite');
+    });
+    test('2st sentence', () => {
+      const output = translator.americanInput('I ate yogurt for breakfast.');
+      assert.include(output, 'yoghurt', 'should change yogurt to yoghurt');
+    });
+    test('3rd sentence', () => {
+      const output = translator.americanInput("We had a party at my friend's condo.");
+      assert.include(output, "flat", 'should change condo to flat');
+    });
+    test('4th sentence', () => {
+      const output = translator.americanInput("Can you toss this in the trashcan for me?");
+      assert.include(output, "bin", 'should change trashcan to bin');
+    });
+    test('5th sentence', () => {
+      const output = translator.americanInput("The parking lot was full.");
+      assert.include(output, "car park", 'should change parking lot to car park');
+    });
+    test('6th sentence', () => {
+      const output = translator.americanInput("Like a high tech Rube Goldberg machine.");
+      assert.include(output, "Heath Robinson device", 'should change machine to device');
+    });
+    test('7th sentence', () => {
+      const output = translator.americanInput("To play hooky means to skip class or work.");
+      assert.include(output, "bunk off", 'should change play hooky to bunk off');
+    });
+    test('8th sentence', () => {
+      const output = translator.americanInput("No Mr. Bond, I expect you to die.");
+      assert.include(output, "Mr Bond", 'should change Mr. to Mr');
+    });
+    test('8th sentence', () => {
+      const output = translator.americanInput("Dr. Grosh will see you now.");
+      assert.include(output, "Dr Grosh", 'should change Dr. to Dr');
+    });
+    test('9th sentence', () => {
+      const output = translator.americanInput("Lunch is at 12:15 today.");
+      assert.include(output, "12.15", 'should change 12:15 to 12.15');
+    });
+  });
 
+  suite('British to American', () => {
+    test('1st sentence', () => {
+      
+    })
+  })
 });
